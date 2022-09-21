7bb032b3f3c429b795b36fcb720bea271442b5b8
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-09-21 18:14:30 +0200

    he creado la clase rectangulo y su metodo constructor

--- ejercicio.py
+++ ejercicio.py
@@ -1,6 +1,7 @@
 from math import sqrt
 from re import X
 from tkinter import Y
+from zlib import DEF_BUF_SIZE
 
 from regex import B
 
@@ -15,13 +16,14 @@ class Punto:
         return "(X={},Y={}".format(self.X,self.Y)
     def cuadrante(self,X,Y):
         if X == 0 and Y != 0:
-            print("Se sitúa sobre el eje Y")
+            print("El punto de coordenadas ({},{}) se sitúa sobre el eje Y")
         elif X != 0 and Y == 0:
-            print("Se sitúa sobre el eje X")
+            print("El punto de coordenadas ({},{}) se sitúa sobre el eje X")
         elif X == 0 and Y == 0:
-            print("Está sobre el origen")
+            print("El punto de coordenadas ({},{}) está sobre el origen")
     def vector(self,punto):
         print("El vector entre {}) y {}) es ({},{})".format(self,punto,punto.X-self.X,punto.Y-self.Y))
+    
 
 A = Punto(2,3)
 B = Punto(5,5)
@@ -30,3 +32,14 @@ D = Punto(0,0)
 
 A.vector(B)
 C.vector(D)
+
+class Rectángulo:
+    inicial = 0
+    final = 0
+    diagonal = (final - inicial)
+    def __init__(self,inicial,final):
+        self.inicial = inicial
+        self.final = final
+    def __str__(self):
+        return "(punto1{},punto2{})".format(self.inicial,self.Y)
+        
