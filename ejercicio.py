9c3efa2a89650a6f59948a73b6b6519dcc94c573
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-09-21 17:58:20 +0200

    he conseguido que funcione

--- ejercicio.py
+++ ejercicio.py
@@ -8,16 +8,11 @@ from regex import B
 class Punto:
     X = 0
     Y = 0
-    def __init__(self,X):
-        X = input()
+    def __init__(self,X,Y):
         self.X = X
-        return X
-    def __init__(self,Y):
-        Y = input()
-        self.Y = Y
-        return Y
+        self.Y = Y   
     def __str__(self):
-        return +str(self.X)+","+str(self.Y)
+        return "(X={},Y={}".format(self.X,self.Y)
     def cuadrante(self,X,Y):
         if X == 0 and Y != 0:
             print("Se sitúa sobre el eje Y")
@@ -26,7 +21,7 @@ class Punto:
         elif X == 0 and Y == 0:
             print("Está sobre el origen")
     def vector(self,punto):
-        print("El vector entre los dos puntos es".format(self,punto,punto.X-self.X,punto.Y-self.Y))
+        print("El vector entre {} y {} es ({},{})".format(self,punto,punto.X-self.X,punto.Y-self.Y))
 
 A = Punto(2,3)
 B = Punto(5,5)
