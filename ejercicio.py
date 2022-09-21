52956844b627024fcbac591b282b48b8a1936522
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-09-21 17:00:52 +0200

    he creado la clase punto y añadido un método constructor

---
+++ ejercicio.py
@@ -0,0 +1,14 @@
+from re import X
+from tkinter import Y
+
+
+class Punto:
+    X = 0
+    Y = 0
+    def __init__(self,X):
+        self.X = X
+        return X
+    def __init__(self,Y):
+        self.Y = Y
+        return Y
+
