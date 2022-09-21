6c14ac0c53067da294d77a360a2acdcfa78a30dd
carmenm02 <91721886+carmenm02@users.noreply.github.com>
2022-09-21 17:17:35 +0200

    he creado el metodo cuadrante

--- ejercicio.py
+++ ejercicio.py
@@ -13,3 +13,13 @@ class Punto:
         return Y
     def __str__(self):
         return +str(self.X)+","+str(self.Y)
+    def cuadrante():
+        if X == 0 and Y != 0:
+            print("Se encuentra en el eje Y")
+        elif X != 0 and Y == 0:
+            print("Se encuentra en el eje X")
+        elif X == 0 and Y == 0:
+            print("Se encuentra en el origen")
+Punto.cuadrante()
+
+
